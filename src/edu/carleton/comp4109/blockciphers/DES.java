2394de13de9251c03ceddc0c6e0f176f4fe653cc
cstubbs <crs.stubbs@gmail.com>
2013-12-12 16:59:27 -0500

    Added commenting to function f

--- src/edu/carleton/comp4109/blockciphers/DES.java
+++ src/edu/carleton/comp4109/blockciphers/DES.java
@@ -37,86 +37,88 @@ public class DES {
 	};
 	
 	
-	static int[][] s1 = {
+	private static int[][] s1 = {
 		{14, 4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
 		{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11,  9,  5,  3,  8},
 		{4, 1, 14,  8, 13,  6, 2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
 		{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
 	 };
 
-	static int[][] s2 = {
+	private static int[][] s2 = {
 			{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
 			{3, 13,  4, 7, 15,  2,  8, 14, 12,  0, 1, 10,  6,  9, 11,  5},
 			{0, 14, 7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15},
 			{13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14,  9}
 		 };
 	
-	static int[][] s3 = {
+	private static int[][] s3 = {
 			{10, 0, 9, 14, 6, 3, 15, 5,  1, 13, 12, 7, 11, 4, 2,  8},
 			{13, 7, 0, 9, 3,  4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
 			{13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14,  7},
 			{1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
 		 };
 	
-	static int[][] s4 = {
+	private static int[][] s4 = {
 			{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
 			{13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14,  9},
 			{10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
 			{3, 15, 0, 6, 10, 1, 13, 8, 9,  4, 5, 11, 12, 7, 2, 14}
 		 };
 	
-	static int[][] s5 = {
+	private static int[][] s5 = {
 			{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
 			{14, 11, 2, 12,  4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
 			{4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
 			{11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
 		  };
 	
-	static int[][] s6 = {
+	private static int[][] s6 = {
 			{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
 			{10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
 			{9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
 			{4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
 		  };
 	
-	static int[][] s7 = {
+	private static int[][] s7 = {
 			{4, 11, 2, 14, 15,  0, 8, 13 , 3, 12, 9 , 7,  5, 10, 6, 1},
 			{13 , 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
 			{1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
 			{6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
 		  };
 	
-	static int[][] s8 = {
+	private static int[][] s8 = {
 			{13, 2, 8,  4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
 			{1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6 ,11, 0, 14, 9, 2},
 			{7, 11, 4, 1, 9, 12, 14, 2,  0, 6, 10 ,13, 15, 3, 5, 8},
 			{2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6 ,11}
 		};
 	
-	static int[][][] s = {s1, s2, s3, s4, s5, s6, s7, s8};
+	private static int[][][] s = {s1, s2, s3, s4, s5, s6, s7, s8};
 	
-	static int[] g = {
-				32, 1, 2, 3, 4, 5,
-				4, 5, 6, 7, 8, 9, 
-				8, 9, 10, 11, 12, 13, 
-				12, 13, 14, 15, 16, 17,
-				16, 17, 18, 19, 20, 21, 
-				20, 21, 22, 23, 24, 25, 
-				24, 25, 26, 27, 28, 29, 
-				28, 29, 30, 31, 32, 1
-			};
+	private static int[] g = 
+	{
+		32,  1,  2,  3,  4,  5,
+		 4,  5,  6,  7,  8,  9, 
+		 8,  9, 10, 11, 12, 13, 
+		12, 13, 14, 15, 16, 17,
+		16, 17, 18, 19, 20, 21, 
+		20, 21, 22, 23, 24, 25, 
+		24, 25, 26, 27, 28, 29, 
+		28, 29, 30, 31, 32,  1
+	};
 		
 	
-	static int[] p = {
-				16, 7, 20, 21, 
-				29, 12, 28, 17, 
-				1, 15, 23, 26, 
-				5, 18, 31, 10, 
-				2, 8, 24, 14, 
-				32, 27, 3, 9, 
-				19, 13, 30, 6, 
-				22, 11, 4, 25
-			};
+	static int[] p = 
+	{
+		16,  7, 20, 21, 
+		29, 12, 28, 17, 
+		 1, 15, 23, 26, 
+		 5, 18, 31, 10, 
+		 2,  8, 24, 14, 
+		32, 27,  3,  9, 
+		19, 13, 30,  6, 
+		22, 11,  4, 25
+	};
 	
 	
 	private long[] K;
@@ -128,6 +130,12 @@ public class DES {
 		
 	}
 	
+	/**
+	 * Encrypt a string message with the DES block cipher 
+	 * @param key
+	 * @param plaintext
+	 * @return
+	 */
 	public String encrypt(String key, String plaintext) {
 		
 		// Build the key schedule
@@ -138,17 +146,23 @@ public class DES {
 		return "";
 	}
 	
-	public BigInteger encrypt(String key, BigInteger plaintext) {
+	/**
+	 * Encrypt a string of numbers
+	 * @param key
+	 * @param plaintext
+	 * @return
+	 */
+	public String encrypt(String key, String plaintext, int radix) {
 		
 		// Build the key schedule
 		buildKeySchedule(hash(key));
-		
 		
 		
 		return null;
 	}
 	
 	private long encryptBlock(long plaintextBlock) {
+		
 		
 		
 		return 0;
@@ -229,58 +243,72 @@ public class DES {
 		}
 	}
 	
+	/**
+	 * Function f in DES algorithm specified in FIPS Pub 46
+	 * @param mi : String - 32-bit message binary string
+	 * @param key : String - 48-bit key binary string
+	 * @return 32-bit output string
+	 */
 	public static String f(String mi, String key) {
-		//function g
+		
+		// Expansion function g
 		String gMi = "";
 		for (int i = 0; i < g.length; i++) {
-		gMi = gMi + mi.charAt(g[i]);
+			gMi = gMi + mi.charAt(g[i]);
 		}
 		
 		long m =  Long.parseLong(gMi, 2);	
 		long k = Long.parseLong(key, 2);
 		
+		// XOR expanded message block and key block (48 bits)
 		Long result = m ^ k;
+		
 		System.out.println(result);
+		
 		String bin = Long.toBinaryString(result);
 		
-		//Making sure the string is 48 bits
+		// Making sure the string is 48 bits
 		while (bin.length() < 48)
-		bin = "0" + bin;
+			bin = "0" + bin;
 		
+		// Split into eight 6-bit strings
 		String[] sin = new String[8];
 		for (int i = 0; i < 8; i++) {
-		sin[i] = bin.substring(0, 6);
-		bin = bin.substring(6);
+			sin[i] = bin.substring(0, 6);
+			bin = bin.substring(6);
 		}
 		
+		// Do S-Box calculations
 		String[] sout = new String[8];
 		for (int i = 0 ; i < 8; i++) {
-		int[][] curS = s[i];
-		String cur = sin[i];
+			int[][] curS = s[i];
+			String cur = sin[i];
 		
-		int row = Integer.parseInt(cur.substring(0, 2), 2); 
-		int col = Integer.parseInt(cur.substring(2, 6), 2);
-		
-		sout[i] = Integer.toBinaryString(curS[row][col]);
-		
-		//Make sure the string is 4 bits
-		while(sout[i].length() < 4)
-			sout[i] = "0" + sout[i];
+			// Get binary values
+			int row = Integer.parseInt(cur.substring(0, 2), 2); 
+			int col = Integer.parseInt(cur.substring(2, 6), 2);
+			
+			// Do S-Box table lookup
+			sout[i] = Integer.toBinaryString(curS[row][col]);
+			
+			// Make sure the string is 4 bits
+			while(sout[i].length() < 4)
+				sout[i] = "0" + sout[i];
 		}
 		
+		// Merge S-Box outputs into one 32-bit string
 		String merged = "";
 		for (int i = 0; i < 8; i++) {
-		merged = merged + sout[i];
+			merged = merged + sout[i];
 		}
 		
-		//Function p 
+		// Apply Permutation P
 		String mergedP = "";
 		for (int i = 0; i < p.length; i++) {
-		mergedP = mergedP + merged.indexOf(p[i]);
+			mergedP = mergedP + merged.indexOf(p[i]);
 		}
 		
 		return mergedP;
-	
 	}
 
 }
